48c6530bf98d033179a4ddf34af761c00feac452
Pete Widders <pwidders@hotmail.com>
2018-11-15 20:39:27 -0800

    got the event listener for user's letter pick and check to see if it matches chosenword working.

--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -1,5 +1,5 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    console.log('Ready!')
+//document.addEventListener("DOMContentLoaded", function (event) {
+//    console.log('Ready!')
 
     //Reference to DOM elements
 
@@ -9,6 +9,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var _guessesLeft = document.getElementById('picks-left');
     var _wins = document.getElementById('wins');
     var _losses = document.getElementById('losses');
+    var _submitButton = document.getElementById('submit-button');
+    var _usersLetterInput = document.getElementById('text-input');
+    var _userKeyEntry = document.getElementsByTagName('body')[0];
 
     //create variables
 
@@ -39,8 +42,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
         _guessesLeft.textContent = guessesLeft;
         _placeholders.textContent = pickedWordPlaceholderArr.join('');
         console.log(pickedWord);
-
-        
     }
 
     function makeTicks(){
@@ -52,50 +53,36 @@ document.addEventListener("DOMContentLoaded", function (event) {
         else {
             pickedWordPlaceholderArr.push('_');
         }
-    }
-    };
+    }};
 
-    //write to DOM
+    // write to DOM
+    
     _guessesLeft.textContent = guessesLeft;
-    _guessedLetters.textContent = incorrectLetterBank;
+    //_guessedLetters.textContent = incorrectLetterBank;
 
-    //user letter guess
+    // Listen for user letter guess. If letter matches a character with chosenWord
+    _submitButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        console.log('I hear you!');
+    // If letter matches a character with chosenWord
+     });
 
-    letterGuess()
-
-    function letterGuess(letter) {
-        if (gameRunning === true && guessedLetterBank.indexOf(letter) === -1) {
-            guessedLetterBank.push(letter);
-        }
-
-        //check if guessed letter is in word
-        for (var i = 0; i < pickedWord.length; i++) {
-            if (pickedWord[i].toLowerCase === letter.toLowerCase()) {
-                pickedWordPlaceholderArr[i] = pickedWord[i];
-                _placeholders.textcontent = pickedWordPlaceholderArr.join('');
-            }
-
-            else {
-                if (gameRunning === false) {
-                    alert("Please click New Game to start");
-                }
-                else {
-                    alert("You've already picked that letter. Please try again.");
+    _userKeyEntry.onkeypress = function (event) {
+        console.log('event!!U@)!@', event)
+        // Check users keypress against chosenWord
+        if (pickedWord.includes(event.key)) {
+            // Iterate through pickedWord to identify matching letter index     
+            for (var i=0; i < pickedWord.length; i++) {
+                if (pickedWord[i] === event.key) {
+                    // i -> 0, 1, 2 ... 7
+                    // pickedWord - cowgirl string
+                    // pickedWord[i] -> 'c', 'o', ... 'l'
+                    pickedWordPlaceholderArr[i] = pickedWord[i]
                 }
             }
-
-            // Check incorrect letter
-            function checkIncorrect(letter) {
-
-            }
-
-            //onkeyup to trigger letterGuess
-            document.onkeyup = function (event) {
-                if (event.keycode >= 65 && event.keycode <= 90) {
-                    letterGuess(event.key);
-                }
-
-            }
+            _placeholders.textContent = pickedWordPlaceholderArr.join('');
         }
     }
-});
+    
+
+
